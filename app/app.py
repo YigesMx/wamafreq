8ed705787a0dd4cf4c053e335170d8494f85e066
BobYue-01 <85574806+BobYue-01@users.noreply.github.com>
2023-06-24 22:23:46 +0800

    app: DCT

--- app/app.py
+++ app/app.py
@@ -11,17 +11,20 @@ import numpy.fft as fft
 from PIL import Image, ImageDraw, ImageFont, ImageColor
 
 import matplotlib.pyplot as plt
-from matplotlib import  rcParams
+from matplotlib import rcParams
+
 
 rcParams['font.family'] = 'monospace'
 rcParams['font.monospace'] = ['Ubuntu Mono', 'JetBrains Mono', 'Consolas', 'monospace']
 
-
 WATER_MARK_ALPHA = 0.04
 TAMPER_LOCATE_CROP = 0.05
+BLOCKS = (32, 32, 3)
+
 
 def crop_handler(image):
     return image, "Crop Done!"
+
 
 def sketch_handler(image, color):
     img, mask = image["image"], image["mask"]
@@ -33,9 +36,11 @@ def sketch_handler(image, color):
     img[mask] = img_sketch[mask]
     return img, "Sketch Done!"
 
+
 def rotate_handler(image, angle):
     img_rot = cv2.warpAffine(image, cv2.getRotationMatrix2D((image.shape[1] // 2, image.shape[0] // 2), angle, 1), (image.shape[1], image.shape[0]))
     return img_rot, "Rotate Done!"
+
 
 def blur_handler(image, blur_size):
     img, mask = image["image"], image["mask"]
@@ -47,13 +52,19 @@ def blur_handler(image, blur_size):
 
     return img, "Blur Done!"
 
+
 def config_handler(wm_alpha, tl_crop):
     WATER_MARK_ALPHA = wm_alpha
     TAMPER_LOCATE_CROP = tl_crop
     return "WATER_MARK_ALPHA = {}\nTAMPER_LOCATE_CROP = {}".format(WATER_MARK_ALPHA, TAMPER_LOCATE_CROP)
 
-def wm_fft(image_ori_input: np.ndarray, watermark_text: str):
 
+def dct_config_handler(blocks_x, blocks_y):
+    BLOCKS = (blocks_x, blocks_y, 3)
+    return "BLOCKS = ({}, {}, 3)".format(BLOCKS[0], BLOCKS[1])
+
+
+def wm_fft(image_ori_input: np.ndarray, watermark_text: str):
     image_ori = np.uint8(image_ori_input.copy())
 
     image_fft = wama_fft.rgb_fft2(image_ori)
@@ -75,6 +86,7 @@ def wm_fft(image_ori_input: np.ndarray, watermark_text: str):
     image_wm = wama_fft.rgb_ifft2(image_wm_fft)
     return utility.center_log_spectrum(image_fft), water_mark, utility.center_log_spectrum(image_wm_fft), image_wm
 
+
 def iwm_fft(image_wm_input: np.ndarray):
     img_wm_fft = wama_fft.rgb_fft2(image_wm_input)
 
@@ -87,13 +99,36 @@ def iwm_fft(image_wm_input: np.ndarray):
     img_tamper_locate = wama_fft.rgb_ifft2(img_wm_fft_crop) * 64
     return utility.center_log_spectrum(img_wm_fft), utility.center_log_spectrum(img_wm_fft_crop), img_tamper_locate
 
+
 def wm_dct(image_ori_input: np.ndarray, watermark_text: str):
-    pass
+    image_ori = np.uint8(image_ori_input.copy())
+    block_size = (image_ori.shape[0] // BLOCKS[0], image_ori.shape[1] // BLOCKS[1])
+    wm_ori = wama_dct.get_feature(image_ori, BLOCKS)
+    img_wm = image_ori.copy()
+    for i in range(wm_ori.shape[0]):
+        for j in range(wm_ori.shape[1]):
+            block = image_ori[i * block_size[0] : (i+1) * block_size[0],
+                            j * block_size[1] : (j+1) * block_size[1], :]
+            block_dct = wama_dct.rgb_dct2(block)
+            block_dct[-1, -1, :] = wm_ori[i, j, :]
+            img_wm[i * block_size[0] : (i+1) * block_size[0],
+                j * block_size[1] : (j+1) * block_size[1], :] = wama_dct.rgb_idct2(block_dct)
+    return wm_ori, img_wm
+
 
 def iwm_dct(image_wm_input: np.ndarray):
-    pass
+    img_fea = wama_dct.get_feature(image_wm_input, BLOCKS)
+    fea_ext = wama_dct.extract(image_wm_input, BLOCKS)
+    fea_ext[fea_ext > 255] = 255
+    fea_ext[fea_ext <   0] = 0
+    fea_ext = np.int16(fea_ext)
+    pos, diff = wama_dct.differ(fea_ext, img_fea)
+    img_marked = wama_dct.mark_diff(image_wm_input, pos, BLOCKS)
+    return img_fea, fea_ext, img_marked
+
 
 with gr.Blocks() as app:
+
     gr.Markdown("# Img Watermark")
 
     with gr.Tab("DFT"):
@@ -123,12 +158,12 @@ with gr.Blocks() as app:
 
                     gr.Markdown("### 预处理")
                     with gr.Row():
-                        image_fft_show = gr.Image(label = "原图频域 (dft)")
+                        image_fft_show = gr.Image(label = "原图频域 (DFT)")
                         water_mark_show = gr.Image(label = "水印")
                     gr.Markdown("### 频域水印添加")
                     with gr.Row():
-                        image_fft_wm_show = gr.Image(label = "原图频域 + 水印 (dft)")
-                        image_wm_show = gr.Image(label = "原图 + 水印 空域 (idft)")
+                        image_fft_wm_show = gr.Image(label = "原图频域 + 水印 (DFT)")
+                        image_wm_show = gr.Image(label = "水印后空域 (IDFT)")
 
         with gr.Accordion("图片篡改小工具",open = False):
 
@@ -161,7 +196,7 @@ with gr.Blocks() as app:
                 image_process_msg = gr.Textbox(placeholder="Output Message", label="输出信息", lines=1, interactive=False)
                 image_process_show = gr.Image(label = "处理后图像")
 
-        with gr.Accordion("水印提取与篡改定位模块",open = False):
+        with gr.Accordion("水印提取与篡改定位模块", open = False):
 
             with gr.Row():
 
@@ -177,10 +212,10 @@ with gr.Blocks() as app:
 
                     gr.Markdown("### 水印显示与篡改信息提取")
                     with gr.Row():
-                        image_wm_fft_show = gr.Image(label = "水印图像频域 (dft)")
+                        image_wm_fft_show = gr.Image(label = "水印图像频域 (DFT)")
                         water_wm_window_show = gr.Image(label = "篡改定位信息窗")
                     gr.Markdown("### 篡改定位显示")
-                    image_tamper_locate_show = gr.Image(label = "原图频域 + 水印 (dft)")
+                    image_tamper_locate_show = gr.Image(label = "原图频域 + 水印 (DFT)")
 
         config_button.click(config_handler, inputs=[wm_alpha, tl_crop], outputs=[config_msg])
 
@@ -193,6 +228,15 @@ with gr.Blocks() as app:
         fft_blur_button.click(blur_handler, inputs=[image_process_blur_input, blur_radius], outputs=[image_process_show, image_process_msg])
 
     with gr.Tab("DCT"):
+
+        with gr.Accordion("DFT 全局参数", open = False):
+
+            gr.Markdown("*以下三个模块的运行请在同一全局参数下进行*")
+            blocks_x = gr.Slider(minimum=0, maximum=32, step=4, label="水平方向分块", value=16)
+            blocks_y = gr.Slider(minimum=0, maximum=32, step=4, label="竖直方向分块", value=16)
+            config_msg = gr.Textbox(placeholder="BLOCKS = ({}, {}, 3)".format(BLOCKS[0], BLOCKS[1]), label="Config Message", lines=1, interactive=False)
+            config_button = gr.Button("Submmit")
+
         with gr.Accordion("水印添加模块", open = False):
 
             with gr.Row():
@@ -202,21 +246,15 @@ with gr.Blocks() as app:
 
                     gr.Markdown("### 图像与水印输入")
                     image_ori_input = gr.Image(label = "上传原图")
-                    watermark_text = gr.Textbox(placeholder="输入水印内容", label="Watermark Content", lines=1)
 
                     wm_dct_button = gr.Button("Make Watermark")
 
                 with gr.Column():
                     gr.Markdown("## 输出")
 
-                    gr.Markdown("### 预处理")
                     with gr.Row():
-                        image_dct_show = gr.Image(label = "原图频域 (dft)")
                         water_mark_show = gr.Image(label = "水印")
-                    gr.Markdown("### 频域水印添加")
-                    with gr.Row():
-                        image_dct_wm_show = gr.Image(label = "原图频域 + 水印 (dft)")
-                        image_wm_show = gr.Image(label = "原图 + 水印 空域 (idft)")
+                        image_wm_show = gr.Image(label = "水印后空域 (DCT)")
 
         with gr.Accordion("图片篡改小工具",open = False):
 
@@ -249,7 +287,7 @@ with gr.Blocks() as app:
                 image_process_msg = gr.Textbox(placeholder="Output Message", label="输出信息", lines=1, interactive=False)
                 image_process_show = gr.Image(label = "处理后图像")
 
-        with gr.Accordion("水印提取与篡改定位模块",open = False):
+        with gr.Accordion("水印提取与篡改定位模块", open = False):
 
             with gr.Row():
 
@@ -265,12 +303,14 @@ with gr.Blocks() as app:
 
                     gr.Markdown("### 水印显示与篡改信息提取")
                     with gr.Row():
-                        image_wm_dct_show = gr.Image(label = "水印图像频域 (dft)")
-                        water_wm_window_show = gr.Image(label = "篡改定位信息窗")
+                        image_wm_dct_show = gr.Image(label = "图像特征")
+                        water_wm_window_show = gr.Image(label = "DCT 水印提取")
                     gr.Markdown("### 篡改定位显示")
-                    image_tamper_locate_show = gr.Image(label = "原图频域 + 水印 (dft)")
+                    image_tamper_locate_show = gr.Image(label = "篡改定位框")
 
-        wm_dct_button.click(wm_dct, inputs=[image_ori_input, watermark_text], outputs=[image_dct_show, water_mark_show, image_dct_wm_show, image_wm_show])
+        config_button.click(dct_config_handler, inputs=[blocks_x, blocks_y], outputs=[config_msg])
+
+        wm_dct_button.click(wm_dct, inputs=[image_ori_input, watermark_text], outputs=[water_mark_show, image_wm_show])
         iwm_dct_button.click(iwm_dct, inputs=[image_wm_input], outputs=[image_wm_dct_show, water_wm_window_show, image_tamper_locate_show])
 
         dct_crop_button.click(crop_handler, inputs=[image_process_crop_input], outputs=[image_process_show, image_process_msg])
@@ -278,4 +318,4 @@ with gr.Blocks() as app:
         dct_rotate_button.click(rotate_handler, inputs=[image_process_rotate_input, rotate_angle], outputs=[image_process_show, image_process_msg])
         dct_blur_button.click(blur_handler, inputs=[image_process_blur_input, blur_radius], outputs=[image_process_show, image_process_msg])
 
-app.launch(share=False)
+app.launch(share=False, server_port=2142)
--- app/wama_dct.py
+++ app/wama_dct.py
@@ -7,66 +7,85 @@ from PIL import Image, ImageDraw, ImageFont
 import matplotlib.pyplot as plt
 from matplotlib import  rcParams
 
-def rgb_dct2(rgbImage:np.ndarray):
+
+def rgb_dct2(rgb_image: np.ndarray):
     '''
-    对rgbImage作dct变换，返回数据类型为float32的频域图像
+    对 rgb_image 作 dct 变换，返回频域图像
     '''
-    return np.transpose([cv2.dct(np.float32(rgbImage[:,:,i])) for i in range(3)],axes=(1,2,0))
-def rgb_idct2(rgbImage_dct:np.ndarray):
+    return np.transpose([cv2.dct(np.float32(rgb_image[:, :, i])) for i in range(3)], axes=(1, 2, 0))
+
+
+def rgb_idct2(rgb_image_dct: np.ndarray):
     '''
-    对rgbImage_idct作逆dct变换，返回数据类型为uint8的空域图像
+    对 rgb_image_dct 作逆 dct 变换，返回空域图像
     '''
-    Img_idct2 = np.transpose([cv2.idct(rgbImage_dct[:,:,i]) for i in range(3)],axes=(1,2,0))
+    Img_idct2 = np.transpose([cv2.idct(rgb_image_dct[:, :, i]) for i in range(3)],
+                             axes=(1, 2, 0))
     Img_idct2_qua = Img_idct2.copy()
-    for i in range(Img_idct2_qua.shape[0]):
-        for j in range(Img_idct2_qua.shape[1]):
-            for k in range(Img_idct2_qua.shape[2]):
-                if(Img_idct2_qua[i,j,k]>255):
-                    Img_idct2_qua[i,j,k]=255
-                elif(Img_idct2_qua[i,j,k]<0):
-                    Img_idct2_qua[i,j,k]=0
-                else:
-                    Img_idct2_qua[i,j,k]=round(Img_idct2_qua[i,j,k])
+    Img_idct2_qua[Img_idct2_qua > 255] = 255
+    Img_idct2_qua[Img_idct2_qua <   0] = 0
     Img_idct2_qua = np.uint8(Img_idct2_qua)
     return Img_idct2_qua
 
-def get_feature(Image:np.ndarray,block_size:tuple):
-    feature = np.zeros(shape=(Image.shape[0]//block_size[0],Image.shape[1]//block_size[1],3))
+
+def get_feature(Image: np.ndarray, blocks: tuple):
+    '''
+    对图像 Image 按 blocks 进行分块，返回每个块的最大值
+    '''
+    block_shape = (Image.shape[0]//blocks[0], Image.shape[1]//blocks[1])
+    feature = np.zeros(
+        shape=(blocks[0], blocks[1], 3))
     for i in range(feature.shape[0]):
         for j in range(feature.shape[1]):
-            feature[i,j,:] = np.max(  Image[block_size[0]*i : block_size[0]*(i+1),block_size[1]*j : block_size[1]*(j+1),:],axis=(0,1))
-    feature = np.uint8((feature - np.min(feature))*255 // (np.max(feature) - np.min(feature)))
+            feature[i, j, :] = np.max(
+                Image[block_shape[0]*i : block_shape[0]*(i+1),
+                      block_shape[1]*j : block_shape[1]*(j+1),
+                      :],
+                axis=(0, 1))
+    feature = np.uint8((feature - np.min(feature))*255 //
+                       (np.max(feature) - np.min(feature)))
     return feature
 
-def differ(img1_fea:np.ndarray,img2_fea:np.ndarray,return_pos=True):
+
+def differ(img1_fea: np.ndarray, img2_fea: np.ndarray, return_pos=True):
     sum = 0
     position = []
     for i in range(img1_fea.shape[0]):
         for j in range(img1_fea.shape[1]):
-            if(np.max(np.abs(img1_fea[i,j,:]-img2_fea[i,j,:])) > 100):
-                sum = sum+1
-                if(return_pos == True):
-                    position.append((i,j))
-    if(return_pos == True):
-        return position,sum/(img1_fea.shape[0]*img1_fea.shape[1])
+            if (np.max(np.abs(img1_fea[i, j, :] - img2_fea[i, j, :])) > 100):
+                sum += 1
+                if (return_pos == True):
+                    position.append((i, j))
+    if (return_pos == True):
+        return position, sum / (img1_fea.shape[0] * img1_fea.shape[1])
     else:
-        return sum/(img1_fea.shape[0]*img1_fea.shape[1])
+        return sum / (img1_fea.shape[0] * img1_fea.shape[1])
 
 
-def extract(Image:np.ndarray,wm_size:tuple):
-    wm_ext = np.zeros(shape=wm_size)
-    Image_dct = rgb_dct2(Image)
-    for i in range(wm_size[0]):
-        for j in range(wm_size[1]):
-            block = Image[i*block_size[0]:(i+1)*block_size[0],j*block_size[1]:(j+1)*block_size[1],:]
+def extract(Image: np.ndarray, blocks: tuple):
+    '''
+    对图像 Image 按 blocks 进行分块，提取每个块的特征水印
+    '''
+    block_size = (Image.shape[0]//blocks[0], Image.shape[1]//blocks[1])
+    wm_ext = np.zeros(shape=blocks)
+    for i in range(blocks[0]):
+        for j in range(blocks[1]):
+            block = Image[i * block_size[0] : (i+1) * block_size[0],
+                          j * block_size[1] : (j+1) * block_size[1], :]
             block_dct = rgb_dct2(block)
-            wm_ext[i,j,:] = block_dct[-1,-1,:] 
+            wm_ext[i, j, :] = block_dct[-1, -1, :]
     return wm_ext
 
-def mark_diff(Image,position,block_shape):
+
+def mark_diff(Image: np.ndarray, position: list, blocks: tuple):
+    '''
+    在图像 Image 上用方框标记 position 对应的的块
+    '''
+    block_size = [Image.shape[0]//blocks[0], Image.shape[1]//blocks[1]]
     Image_marked = Image.copy()
     for pos in position:
-        start_point = (pos[1]*block_shape[0],pos[0]*block_shape[1])
-        end_point = (pos[1] * block_shape[0] + block_shape[0] - 4, pos[0] * block_shape[1] + block_shape[1] - 4)
-        cv2.rectangle(Image_marked, start_point, end_point, (0, 0, 255), thickness=2, lineType=cv2.LINE_8)
+        start_point = (pos[1] * block_size[1] + 2, pos[0] * block_size[0] + 2)
+        end_point = (pos[1] * block_size[1] + block_size[1] - 2, pos[0] * block_size[0] + block_size[0] - 2)
+        cv2.rectangle(Image_marked, start_point, end_point,
+                      (0, 0, 255), thickness=2, lineType=cv2.LINE_8)
     return Image_marked
